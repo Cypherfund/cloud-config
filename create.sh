e579dd1d221408cf74bd7e3b1e81b0f98d284988
Ngai Elizabeth <asobingai@gmail.com>
2023-02-19 01:40:52 +0100

    add configs

---
+++ create.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+touch account-service.properties
+touch account-service-stage.properties
+touch account-service-prod.properties
+
+touch bbn-service.properties
+touch bbn-service-stage.properties
+touch bbn-service-prod.properties
+
+touch configuration-service.properties
+touch configuration-service-stage.properties
+touch configuration-service-prod.properties
+
+touch lottery-service.properties
+touch lottery-service-stage.properties
+touch lottery-service-prod.properties
+
+touch microtask-service.properties
+touch microtask-service-stage.properties
+touch microtask-service-prod.properties
+
+touch notification-service.properties
+touch notification-service-stage.properties
+touch notification-service-prod.properties
+
+touch payment-service.properties
+touch payment-service-stage.properties
+touch payment-service-prod.properties
+
+touch support-service.properties
+touch support-service-stage.properties
+touch support-service-prod.properties
+
+touch user-service.properties
+touch user-service-stage.properties
+touch user-service-prod.properties
